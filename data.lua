e6997dd625f3d79dd5223bb5a3cec98d626b1610
ShadowyXPGames <shadowyxpgaming@gmail.com>
2017-04-14 22:31:11 -0500

    added wasp code

--- data.lua
+++ data.lua
@@ -48,3 +48,37 @@ data.raw["logistic-robot"]["logistic-robot"].working_sound = {
     audible_distance_modifier = 0.5,
     probability = 1 / (1 * 60) -- average pause between the sound is 1 seconds
 }
+
+data.raw["consruction-robot"]["construction-robot"].idle = {
+	filename = "__Robees__/graphics/wasp.png",
+	priority = "high",
+	line_length = 16,
+	width = 32,
+	height = 36,
+	frame_count = 1,
+	shift = {0, -0.15625},
+	direction_count = 16
+}
+data.raw["consruction-robot"]["construction-robot"].in_motion = {
+	filename = "__Robees__/graphics/wasp.png",
+	priority = "high",
+	line_length = 16,
+	width = 32,
+	height = 36,
+	frame_count = 1,
+	shift = {0, -0.15625},
+	direction_count = 16,
+	y = 36
+}
+data.raw["consruction-robot"]["construction-robot"].working =
+    {
+      filename = "__Robees__/graphics/wasp_working.png",
+      priority = "high",
+      line_length = 2,
+      width = 28,
+      height = 36,
+      frame_count = 2,
+      shift = {0, -0.15625},
+      direction_count = 16,
+      animation_speed = 0.3,
+    }
